549cdaf47494c01877a8c266112bffbcc3ea0f49
Ankitajayale <138603085+Ankitajayale@users.noreply.github.com>
2024-03-19 05:48:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ store.on("error", (err) => {
 
 const sessionOptions = {
   store,
-  secret:process.env.SECRET;
+  secret:process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
